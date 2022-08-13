7fa5b0fa8f152759d587fdf39a953128d4e25825
pyjonathan <jonathan.r.buchanan@gmail.com>
2022-08-13 10:53:32 -0400

    add user input ip

--- section_1_script.py
+++ section_1_script.py
@@ -5,7 +5,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-ip = "8.8.8.8"
+ip = input("Enter IP to Lookup: ")
 
 url = f"https://ipapi.co/{ip}/json/"
 
